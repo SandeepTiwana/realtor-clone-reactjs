25aa642314e7572731cc8003f659484605802d69
sandeep <sstiwana1997@gmail.com>
2023-04-10 10:48:22 +0530

    header changes retracement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ const Header = () => {
   
   const location = useLocation();
 const navigate = useNavigate();
+  
   function pathMatchRoute(route){
     if(route === location.pathname){
       return true
@@ -18,7 +19,7 @@ const navigate = useNavigate();
         <div>
           <ul className='flex space-x-10'>
             <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"} `} onClick={()=> navigate("/")} >Home</li>
-            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/Offers") && "text-black border-b-red-500"} `} onClick={()=> navigate("/Offers")}>Offers</li>
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/Offers") && 'text-black border-b-red-500'} `} onClick={()=> navigate("/Offers")}>Offers</li>
             <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/SignIn") && "text-black border-b-red-500"} `} onClick={()=> navigate("/SignIn")}>Sign in</li>
           </ul>
         </div>
